1c9e1d2960c00908627364768fb39c022dba59e9
Léo Pictet <leo@pictet.net>
2020-08-13 15:15:51 +0200

    Going slowly to somewhere

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Mint")
+Ingredient.create(name: "Rhum")
+Ingredient.create(name: "Ginger")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Blueberries")
+Ingredient.create(name: "Watermelon")
+Ingredient.create(name: "Sugar")
+Ingredient.create(name: "Salt")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Panama Beach")
+Cocktail.create(name: "Blue Spark")
+Cocktail.create(name: "Redonin")
+Cocktail.create(name: "Asian Flower")
+Cocktail.create(name: "Turk Gerber")
+
