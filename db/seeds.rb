36d678240195ae6283d5ab75bb9d9b370cc37653
Léo Pictet <leo@pictet.net>
2020-08-14 13:00:42 +0200

    Youpi

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.destroy_all
 Cocktail.destroy_all
+Ingredient.destroy_all
 
 Ingredient.create(name: "Lemon 🍋")
 Ingredient.create(name: "Ice 🧊")
